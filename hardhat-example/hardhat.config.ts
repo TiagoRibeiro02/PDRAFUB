edff7fd10e762e460282baea3622dda0363d471a
Tiago Ribeiro <95197303+TiagoRibeiro02@users.noreply.github.com>
2026-09-11 13:08:58 +0100

    deleted unused hardhat example

--- hardhat-example/hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "hardhat/config";
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-
-export default defineConfig({
-  plugins: [hardhatToolboxViemPlugin],
-  solidity: {
-    version: "0.8.28",
-  },
-});
